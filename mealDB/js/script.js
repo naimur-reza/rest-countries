c77dfbae2a6e95efde95ae0517d93dcdceffc749
Naimur Reza <naimur.rezaa@gmail.com>
2023-02-24 19:51:03 +0600

    completed dynamic search and modal

--- mealDB/js/script.js
+++ mealDB/js/script.js
@@ -18,11 +18,14 @@ const displayMils = meals => {
         <h5 class="card-title">${meal.strMeal}</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in
             to additional content. This content is a little bit longer.</p>
+    
+            <button onclick="loadMealDetail(${meal.idMeal})" type="button" class="btn btn-primary" data-toggle="modal" data-target="#mealsModal">
+            More details
+          </button>
     </div>
-</div>
+    </div>
     `
     mealsContainer.appendChild(newDiv)
-    console.log(meal);
   })
 }
 
@@ -35,3 +38,26 @@ const searchMeals = () => {
   loadMeals(searchField)
 }
 loadMeals('fish')
+
+//loads meals details
+const loadMealDetail = (idMeal) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+  fetch(url)
+  .then(res => res.json())
+  .then(detail => displayMealDetails(detail.meals[0]))
+}
+
+const displayMealDetails = (data) =>{
+  const title = document.getElementById('mealsModalLabel')
+  title.innerText = data.strMeal;
+  const mealDetailsBody = document.getElementById('mealDetailsBody')
+  mealDetailsBody.innerHTML = `
+  <img class="img-fluid" src="${data.strMealThumb}" >
+  <h5 class="mt-2">Category : ${data.strCategory} </h5>
+  <h5 class="mt-2">Area : ${data.strArea} </h5>
+  <h6>Recipe : ${data.strIngredient1},${data.strIngredient2},${data.strIngredient3},${data.strIngredient4},${data.strIngredient5},${data.strIngredient6}
+
+  `
+console.log(data);
+  
+}
